7b9376caae0e8b3479b9cc4397b21d9665cc3bee
anastalal <anasctalal@gmail.com>
2023-06-24 18:33:29 +0300

    first commit

---
+++ precache-manifest.a7fe81aec575307cf71261840d87aa1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a99d771150a25e1efc414acf7d69ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ae861509bd25781c6a",
+    "url": "/static/js/2.14d80db7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.14d80db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a3ba58bf5585710e7f",
+    "url": "/static/js/main.0d5eac71.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0d5eac71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd5a6c31946c4eb561d",
+    "url": "/static/js/runtime-main.a50a225d.js"
+  }
+]);
